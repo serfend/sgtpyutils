a4e1a8b1281653d09426ba738fb7490f57ef5a7b
汉广 <serfend@foxmail.com>
2022-06-05 18:38:34 +0800

    feat[ci]init

--- sgtpyutils/__version__.py
+++ sgtpyutils/__version__.py
@@ -1,8 +1,8 @@
 __title__ = "sgtpyutils"
-__description__ = "sgtpyutils is a public library."
+__description__ = "sgtpyutils is a public library for use."
 __keywords__ = ['lib', 'utils']
 __url__ = "https://github.com/serfend/sgtpyutils"
-__version__ = "1.06.0"
+__version__ = "1.0.0"
 __author__ = "serfend"
 __author_email__ = "serfend@foxmail.com"
 __license__ = "MIT Licence"
