52ee932dba9c98add6ea690b5f04229fa14158ad
汉广 <serfend@foxmail.com>
2022-06-07 09:21:39 +0800

    feat[service]return add \n

--- sgtpyutils/network/SimpleCaculateService/__init__.py
+++ sgtpyutils/network/SimpleCaculateService/__init__.py
@@ -7,7 +7,7 @@ class SimpleCaculateService(SimpleTcpService):
             result = eval(data.decode())
         except Exception as e:
             result = f'Error: {e}'
-        self.server.send(str(result).encode())
+        self.server.send(f'{result}\n'.encode())
 
     def __init__(self, host: str = '127.0.0.1'):
         return super().__init__(self.handle_caculate, host)
