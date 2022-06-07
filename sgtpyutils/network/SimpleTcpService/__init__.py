1e5ad7efafe981ac3612cc9bac584e1fb8d9c116
汉广 <serfend@foxmail.com>
2022-06-07 08:27:20 +0800

    fix[listening]check status

--- sgtpyutils/network/SimpleTcpService/__init__.py
+++ sgtpyutils/network/SimpleTcpService/__init__.py
@@ -15,7 +15,8 @@ class SimpleTcpService(threading.Thread):
         min_port = int(1001)
         if not port:
             s_port = random.Random().randint(min_port, max_port)
-        while True:
+        max_try_time = 1e3
+        while max_try_time > 0:
             '''
             get a free port
             '''
@@ -26,15 +27,17 @@ class SimpleTcpService(threading.Thread):
                 s_port += 1
                 if s_port >= int(max_port):
                     s_port = min_port
-                time.sleep(0.5)
+                max_try_time -= 1
 
         self.socket = server
         return s_port
 
     def run(self) -> None:
         logger.debug(f'server start listening on {self.host}:{self.port}')
+        self.__on_listen(True)
         self.socket.listen(1)
         self.server, self.remote_host = self.socket.accept()
+        self.__on_listen(False)
         logger.debug(f'connection establish from {self.remote_host}')
         super().run()
         while self.running:
@@ -45,14 +48,25 @@ class SimpleTcpService(threading.Thread):
             if self.on_message:
                 self.on_message(data)
 
+    def __on_listen(self, is_start: bool):
+        self.is_listening = is_start
+        if is_start and self.on_listen_start:
+            self.on_listen_start()
+        elif not is_start and self.on_listen_end:
+            self.on_listen_end()
+
     def __init__(self, on_message: Callable, host: str = '127.0.0.1', port: int = None):
+        self.is_listening = False  # whether self-listening
         self.on_message: Callable = on_message
+        self.on_listen_start: Callable = None
+        self.on_listen_end: Callable = None
         self.socket: socket.socket = None
         self.server: socket.socket
         self.host = host
         self.running = False
         self.port = self.generate_free(port)
         super().__init__()
+        self.daemon = True  # as daemon thread default
 
     def start(self) -> None:
         self.running = True
--- tests/test_network.py
+++ tests/test_network.py
@@ -1,13 +1,25 @@
 import socket
 import random
+import time
+from sgtpyutils.logger import logger
 from sgtpyutils.network.SimpleCaculateService import SimpleCaculateService
 
 
-def test_caculator():
+def test_service_start():
     s = SimpleCaculateService()
-    s.setDaemon(True)
     s.start()
+    counter = 100
+    while not s.is_listening and counter:
+        time.sleep(0.1)
+        counter -= 1
+    assert s.is_listening == True
+    return s
+
+
+def test_caculator():
+    s = test_service_start()
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    logger.info(f'start connect to local:{s.port}')
     client.connect(('127.0.0.1', s.port))
     rnd = random.Random()
     for i in range(10):
@@ -19,4 +31,3 @@ def test_caculator():
         c = a + b
         print(f'get result for {a} + {b} = {c} , recv {data}')
         assert c == data
-    
