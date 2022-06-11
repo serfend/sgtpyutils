3d4d0d8c0c5b0df91aea97ca1894f684c11f3d79
汉广 <serfend@foxmail.com>
2022-06-11 19:27:25 +0800

    feat[xls]dict convert doc

--- sgtpyutils/xls_txt/dict/__init__.py
+++ sgtpyutils/xls_txt/dict/__init__.py
@@ -4,12 +4,16 @@ from typing import Callable, Dict
 def dict2sheet(data: Dict, formatter: Callable = None, header: dict = {'key': 'value', '-'*20: '-'*30}, margin: int = 30):
     '''
       convert key-value to sheet
-      if formatter is set , no need to set margin
+      formatter: Callable[line:str,lines_dict:Dict] -> str : specify how to format a line
+            default set is lambda x,dic:f'{left_just(x)}\t{hex(dic[x]) if isinstance(dic[x],int) else dic[x]}'
+            if formatter is set , no need to set margin
+      
     '''
     result = dict(header)
     result.update(data)
 
     def left_just(x: str):
+        x = str(x)
         return x.ljust(margin, ' ')
     if not formatter:
         def formatter(
