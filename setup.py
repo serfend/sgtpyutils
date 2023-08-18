c5106340c0e482cd2cf31e8e11d62dc4b97cb55f
汉广 <serfend@foxmail.com>
2023-08-18 11:28:34 +0800

    publish[1.17.2]

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     package_data={'': ['LICENSE', 'NOTICE']},
     package_dir=pck_dict,
     include_package_data=True,
-    python_requires='>=3.7, <4',
+    python_requires='>=3.10, <4',
     license=about['__license__'],
     zip_safe=False,
     classifiers=[
@@ -72,9 +72,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3 :: Only',
