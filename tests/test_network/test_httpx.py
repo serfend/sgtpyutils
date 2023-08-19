a2bde8bf013427d40c351026d4a22587edd2566c
汉广 <serfend@foxmail.com>
2023-08-19 10:26:26 +0800

    fix[test-http]use https

--- tests/test_network/test_httpx.py
+++ tests/test_network/test_httpx.py
@@ -1,6 +1,6 @@
 from sgtpyutils.network.httpx import *
 import asyncio
 def test_baidu():
-  data = asyncio.run(get_url('http://baidu.com'))
+  data = asyncio.run(get_url('https://baidu.com'))
   assert len(data)
   assert 'baidu' in data
