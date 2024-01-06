3e588a46f34072625299bb9082adc04530ebf464
汉广 <serfend@foxmail.com>
2024-01-06 21:35:18 +0800

    feat[database]support batch-delete

--- sgtpyutils/database/filebase_database/__init__.py
+++ sgtpyutils/database/filebase_database/__init__.py
@@ -4,7 +4,7 @@ import os
 import json
 import atexit
 from sgtpyutils.datetime import DateTime
-from sgtpyutils.functools import *
+from sgtpyutils.functools import AssignableArg
 
 
 class DateEncoder(json.JSONEncoder):
@@ -18,25 +18,35 @@ class DateEncoder(json.JSONEncoder):
 class Database:
     cache: dict[str, dict] = {}
     root_path: str = None
+    raw_value: dict
+    '''数据原始对象'''
+    database: str
+    '''数据库名称'''
 
     def __init__(self, database: str) -> None:
         atexit.register(self.save)
         self.database = database
 
-        self.base_path = f'{Database.root_path}{os.sep}'
         self.load_db()
 
     def check_file(self):
-        xxx = pathlib2.Path(self.database_filename)
+        return Database.ensure_file(self.database_filename)
+
+    @staticmethod
+    def ensure_file(path: str):
+        xxx = pathlib2.Path(path)
         if not xxx.parent.is_dir():
             os.makedirs(xxx.parent.as_posix())
         if not xxx.exists():
-            with open(xxx.as_posix(), 'w') as f:
+            with open(xxx.as_posix(), 'w'):
                 pass
 
     def load_db(self, reload: bool = False):
-        if not reload and Database.cache.get(self.database):
-            return Database.cache.get(self.database)
+        if not reload:
+            if Database.cache.get(self.database) is not None:
+                self.raw_value = Database.cache.get(self.database)
+                return self.raw_value
+        
         self.check_file()
         with open(self.database_filename, 'r', encoding='utf-8') as f:
             data = f.read()
@@ -44,25 +54,62 @@ class Database:
                 data = '{}'
             self.raw_value = json.loads(data)
         Database.cache[self.database] = self.raw_value
+        return self.raw_value
 
     def save(self):
-        self.check_file()
+        if hasattr(self, 'is_deleted'):
+            return False
+        if Database.cache.get(self.database) is None:
+            return  # 已被删除
+        return Database.save_direct(self.database_filename, self.raw_value)
 
-        with open(self.database_filename, 'w', encoding='utf-8') as f:
-            data = self.value
+    @staticmethod
+    def save_direct(path: str, data: dict):
+        Database.ensure_file(path)
+
+        with open(path, 'w', encoding='utf-8') as f:
             data_raw = json.dumps(data, cls=DateEncoder)
             f.write(data_raw)
 
     @property
     def database_filename(self) -> str:
-        return f'{self.base_path}{self.database}.json'
+        '''文件完整路径'''
+        return Database._database_filename(self.database, self.base_path)
+
+    def _database_filename(data: str, base: str = None) -> str:
+        if base is None:
+            base = Database.get_base_path(data)
+        return f'{base}{data}.json'
+
+    @property
+    def base_path(self):
+        if hasattr(self, '_basepath'):
+            return self._basepath
+        self._basepath = Database.get_base_path(self.database)
+        return self._basepath
+
+    @staticmethod
+    def get_base_path(database: str):
+        base_path: str
+        '''基础路径，当使用相对路径时使用'''
+        is_abs: bool
+        '''是否是绝对路径，影响base_path'''
+
+        is_abs = pathlib2.Path(database).is_absolute()
+        if is_abs:
+            base_path = ''
+        else:
+            current = pathlib2.Path(__file__).parent.parent.parent
+            root_path = current.joinpath('database')
+            base_path = f'{root_path}{os.sep}'
+        return base_path
 
     @property
     def value(self) -> dict:
         return Database.cache[self.database]
 
     def get(self, key: str, default: any = None) -> dict:
-        if not key in self.value:
+        if key not in self.value:
             self.value[key] = default
         return self.value.get(key)
 
@@ -77,6 +124,23 @@ class Database:
                 return target_mathod(*arg.args, **arg.kwargs)
             return wrapper
         return func
+
+    @staticmethod
+    def save_all():
+        for x in Database.cache:
+            data = Database.cache[x]
+            path = Database._database_filename(x)
+            Database.save_direct(path, data)
+
+    def delete(self):
+        self.is_deleted = True
+        if Database.cache.get(self.database) is not None:
+            del Database.cache[self.database]
+
+        path = self.database_filename
+        if not os.path.exists(path):
+            return
+        os.remove(path)
 
 
 __x = pathlib2.Path(__file__)
--- tests/test_database.py
+++ tests/test_database.py
@@ -15,10 +15,50 @@ def test_db():
     db = filebase_database.Database('test')
     assert db.value['test'] == 'test'
 
+    db.delete()
+
 
 def test_aop():
-    @filebase_database.Database.require_database('test')
+    @filebase_database.Database.require_database('test_aop')
     def test(database: filebase_database.Database):
         assert database
         assert database.database_filename
+        database.delete()
     test()
+
+
+def test_save_all():
+    for i in range(10):
+        db_name = f'test_save_all_{i}'
+        db = filebase_database.Database(db_name)
+        db.value['x'] = i
+
+    filebase_database.Database.save_all()
+
+    for i in range(10):
+        db_name = f'test_save_all_{i}'
+        db = filebase_database.Database(db_name)
+        assert db.value['x'] == i
+
+    for i in range(10):
+        db_name = f'test_save_all_{i}'
+        db = filebase_database.Database(db_name)
+        db.delete()
+        assert not os.path.exists(db.database_filename)
+
+def test_multi_edit():
+    db_name = 'test_multi_edit'
+    db = filebase_database.Database(db_name)
+    db.value['common'] = 1
+
+    db2 = filebase_database.Database(db_name)
+    db2.value['common'] += 1
+
+    db3 = filebase_database.Database(db_name)
+    db3.value['common2'] = '123'
+
+    db_check = filebase_database.Database(db_name)
+    assert db_check.value['common'] == 2
+    assert db_check.value['common2'] == '123'
+
+    db_check.delete()
