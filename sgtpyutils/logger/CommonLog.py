4f0cf98b0a3090836f9bae4d278bd3b216d096d0
汉广 <serfend@foxmail.com>
2024-08-28 02:05:27 +0800

    publish[1.20.4]

---
+++ sgtpyutils/logger/CommonLog.py
@@ -0,0 +1,26 @@
+import pathlib2
+import logging
+import logging.handlers
+
+
+class CommpnLog(logging.Logger):
+    @classmethod
+    def create(
+        cls,
+        level=logging.INFO,
+        log_dir='log/common.log',
+        fmt: str = '%(asctime)s:%(message)s',
+    ):
+        self = logging.getLogger(log_dir)
+        self.setLevel(level)  # 设置日志级别
+        path = pathlib2.Path(log_dir)
+        if path.parent.parts and not path.parent.exists():
+            path.parent.mkdir(parents=True)
+        file_handler = logging.handlers.RotatingFileHandler(path.as_posix(), maxBytes=1024 * 1024, backupCount=5)
+        formatter = logging.Formatter(fmt)  # 实例化formatter。
+        stream_handler = logging.StreamHandler()  # 往屏幕上输出
+        stream_handler.setFormatter(formatter)
+        file_handler.setFormatter(formatter)
+        self.addHandler(stream_handler)
+        self.addHandler(file_handler)
+        return self
