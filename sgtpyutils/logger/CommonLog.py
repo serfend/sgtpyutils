229f05b91742c06e0495874976cafe505b38ddc1
汉广 <serfend@foxmail.com>
2025-01-18 11:38:38 +0800

    typo[log]

--- sgtpyutils/logger/CommonLog.py
+++ sgtpyutils/logger/CommonLog.py
@@ -2,21 +2,22 @@ import pathlib2
 import logging
 import logging.handlers
 
-
-class CommpnLog(logging.Logger):
+class CommonLog(logging.Logger):
     @classmethod
     def create(
         cls,
         level=logging.INFO,
         log_dir='log/common.log',
         fmt: str = '%(asctime)s:%(message)s',
+        maxBytes=1024 * 1024,
+        backupCount=5,
     ):
         self = logging.getLogger(log_dir)
         self.setLevel(level)  # 设置日志级别
         path = pathlib2.Path(log_dir)
         if path.parent.parts and not path.parent.exists():
             path.parent.mkdir(parents=True)
-        file_handler = logging.handlers.RotatingFileHandler(path.as_posix(), maxBytes=1024 * 1024, backupCount=5)
+        file_handler = logging.handlers.RotatingFileHandler(path.as_posix(), maxBytes=maxBytes, backupCount=backupCount)
         formatter = logging.Formatter(fmt)  # 实例化formatter。
         stream_handler = logging.StreamHandler()  # 往屏幕上输出
         stream_handler.setFormatter(formatter)
@@ -24,3 +25,7 @@ class CommpnLog(logging.Logger):
         self.addHandler(stream_handler)
         self.addHandler(file_handler)
         return self
+
+
+class CommpnLog(CommonLog):
+    pass
